b41f7b9f7086bdc41caab798dc0ed47f528b769a
sshifty <atiszon@gmail.com>
2019-02-20 18:07:47 +0100

    Fixed concurrently script

---
+++ client/src/Components/ChatScreen.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ChatScreen extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default ChatScreen;
